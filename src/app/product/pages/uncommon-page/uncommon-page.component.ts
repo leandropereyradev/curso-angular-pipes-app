a78cbe858a69d6aaa7ce707707c9b90331ae2d57
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-24 15:40:34 +0200

    UncommonPage: i18nSelect

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Leandro';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Laura';
+    this.gender = 'female';
+  }
 }
