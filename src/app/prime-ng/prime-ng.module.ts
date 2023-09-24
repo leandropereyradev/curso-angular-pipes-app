7cd054696009993da480f7adf8f4ad5a4e1273f3
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-24 19:41:58 +0200

    OrderComponent: PrimeNG - Table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,7 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   exports: [
@@ -14,6 +15,7 @@ import { ToolbarModule } from 'primeng/toolbar';
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    TableModule,
     ToolbarModule,
   ],
 })
