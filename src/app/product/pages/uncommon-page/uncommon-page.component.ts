b98ac94d1c2539300e4521e1033005f4eab9e0db
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-24 15:50:44 +0200

    UncommonPage: i18nPlural

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -18,4 +18,24 @@ export class UncommonPageComponent {
     this.name = 'Laura';
     this.gender = 'female';
   }
+
+  // i18n Plural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+  public clientsMap = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos un cliente esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
 }
